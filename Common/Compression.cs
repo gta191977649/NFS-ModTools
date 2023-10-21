8d753b958ab6d49957b21dd6cb3bce04cfd574ab
Nurupo <191977649@qq.com>
2023-10-22 08:33:36 +1100

    Attempted custom implmented NFSTools.CompLib

--- Common/Compression.cs
+++ Common/Compression.cs
@@ -1,9 +1,33 @@
 ﻿using System;
 using System.IO;
-using CompLib;
+//using CompLib;
+using System.IO.Compression;
+
 
 namespace Common
 {
+    public static class BlobDecompressor
+    {
+        public static Span<byte> Decompress(ReadOnlySpan<byte> input)
+        {
+            using MemoryStream inputMemoryStream = new MemoryStream(input.ToArray());
+            using GZipStream gzipStream = new GZipStream(inputMemoryStream, CompressionMode.Decompress);
+            using MemoryStream outputMemoryStream = new MemoryStream();
+            gzipStream.CopyTo(outputMemoryStream);
+            return new Span<byte>(outputMemoryStream.ToArray());
+        }
+    }
+    
+    public static class CipDecompressor
+    {
+        public static void Decompress(Stream src, Stream dst, long srcSize, out long decompressedSize)
+        {
+            using GZipStream gzipStream = new GZipStream(src, CompressionMode.Decompress);
+            gzipStream.CopyTo(dst);
+            decompressedSize = dst.Length;
+        }
+    }
+    
     /// <summary>
     /// Wrapper around CompLib API 
     /// </summary>
